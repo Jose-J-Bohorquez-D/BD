cdddb4ad6cb171f9ebd413e165710f84e8cc0834
Jose Bohorquez <bd567358546@gmail.com>
2022-05-07 16:40:06 -0500

    se reemplaza el js del sweetalert2 por un error en la consola dv del navegador

--- Vistas/plantilla.php
+++ Vistas/plantilla.php
@@ -44,7 +44,7 @@ session_start();
   <!-- DataTables  & Plugins --> <script src="Vistas/plugins/datatables/jquery.dataTables.min.js"></script> <script src="Vistas/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script> <script src="Vistas/plugins/datatables-responsive/js/dataTables.responsive.min.js"></script> <script src="Vistas/plugins/datatables-responsive/js/responsive.bootstrap4.min.js"></script> <script src="Vistas/plugins/datatables-buttons/js/dataTables.buttons.min.js"></script> <script src="Vistas/plugins/datatables-buttons/js/buttons.bootstrap4.min.js"></script> <script src="Vistas/plugins/jszip/jszip.min.js"></script> <script src="Vistas/plugins/pdfmake/pdfmake.min.js"></script> <script src="Vistas/plugins/pdfmake/vfs_fonts.js"></script> <script src="Vistas/plugins/datatables-buttons/js/buttons.html5.min.js"></script> <script src="Vistas/plugins/datatables-buttons/js/buttons.print.min.js"></script> <script src="Vistas/plugins/datatables-buttons/js/buttons.colVis.min.js"></script>
   <!-- AdminLTE App --><script src="Vistas/dist/js/adminlte.min.js"></script>
   <!-- AdminLTE for demo purposes <script src="Vistas/dist/js/demo.js"></script>-->
-  <!-- sweetAlert2 --><script src="Vistas/js/sweetalert2.all.min.js"></script>
+  <!-- sweetAlert2 --><script src="Vistas/Js/sweetalert2-07052022.all.min.js"></script>
 
   </head>
   <?php if (isset($_SESSION['iniciarSesion']) && $_SESSION['iniciarSesion'] == "ok") {
